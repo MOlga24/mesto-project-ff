e27816bf1bd6d6ac8b10750c87624bb7bd7c4939
Olga M <ol.unya@yandex.ru>
2024-07-24 22:27:32 +0500

    add and delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const container = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+function createCard(item, removeCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__title").textContent = item.name;
+  deleteButton = cardElement.querySelectorAll(".card__delete-button");
+  deleteButton.forEach((elem) => {
+    elem.addEventListener("click", removeCard);
+  });
+  container.append(cardElement);
+  return cardElement;
+}
+function removeCard() {
+  let listItem = this.closest(".card");
+  listItem.remove();
+}
+function render() {
+  initialCards.forEach(function (item) {
+    createCard(item, removeCard);
+  });
+}
+render();
