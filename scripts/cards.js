1bef93c189201ed012fa7c3b0f0ae70790ece6a5
Olga M <ol.unya@yandex.ru>
2024-08-01 22:43:15 +0500

    реализованы добавления карточек через попап

--- scripts/cards.js
+++ scripts/cards.js
@@ -11,16 +11,16 @@ const initialCards = [
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
     },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+    // {
+    //   name: "Камчатка",
+    //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    // },
+    // {
+    //   name: "Холмогорский район",
+    //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    // },
+    // {
+    //   name: "Байкал",
+    //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    // }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,16 @@
 // @todo: Вывести карточки на страницу
 const container = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
-
+const popup = document.querySelector('.popup_type_new-card');
+const addButton = document.querySelector(".profile__add-button");
+const popupForm = popup.querySelector('.popup__form');
+const saveButton = popupForm.querySelector('.popup__button');
+const popupOpener = document.querySelector('.profile__add-button');
+const popupCloser = popup.querySelector('.popup__close');
+let initialCardsCopy = initialCards;
+// for (let elem of initialCards) {
+//   initialCardsCopy.push(elem);
+//  }
 function createCard(item, removeCard) {
   const cardImage = cardTemplate.querySelector(".card__image");
   cardImage.setAttribute("alt", item.name);
@@ -18,10 +27,15 @@ function createCard(item, removeCard) {
     .querySelector(".places__item")
     .cloneNode(true);
   cardElement.querySelector(".card__title").textContent = item.name;
+  cardElement.querySelector(".card__like-button").addEventListener("click", function (evt) { 
+    evt.target.classList.toggle("card__like-button_is-active");
+    
+   });
   const deleteButoon = cardElement.querySelector(".card__delete-button");
   deleteButoon.addEventListener("click", function () {
     removeCard(deleteButoon);
   });
+  
   return cardElement;
 }
 
@@ -30,9 +44,51 @@ function removeCard(deleteButoon) {
   listItem.remove();
 }
 
-function render() {
+function render(initialCards) {
+container.innerHTML = " ";
   initialCards.forEach(function (item) {
     container.append(createCard(item, removeCard));
   });
 }
-render();
+
+popupOpener.addEventListener('click', openPopapAndLockScroll);
+function openPopapAndLockScroll() {    
+
+  popup.classList.add("popup_is-opened");
+  
+  popupCloser.addEventListener('click', close);  
+
+  popupForm.addEventListener('submit', addCard);  
+  
+
+
+     
+}
+function addCard(event) { 
+   
+  
+  const name = popupForm.querySelector(".popup__input_type_card-name").value;
+ const link = popupForm.querySelector(".popup__input_type_url").value;
+
+  event.preventDefault();
+  initialCardsCopy.push({name, link});
+
+
+
+close();
+
+   }
+// function returnScroll() {
+//   document.body.classList.remove('scroll-lock');
+// }
+
+function close() {
+ 
+  popup.classList.remove("popup_is-opened");
+  render(initialCardsCopy);
+
+  // returnScroll();
+}
+
+
+  render(initialCards);
